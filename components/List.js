c382a967f701e802b9d4e74fc15ffc56d4c3a7af
Michael Caputo <mike@capu.to>
2019-12-03 05:26:32 -0800

    Add feed

---
+++ components/List.js
@@ -0,0 +1,19 @@
+import { FlatList } from 'react-native';
+import FormButton from './FormButton';
+import ListItem from './ListItem';
+import React from 'react';
+
+const List = ({ onPressFooter, ...props }) => {
+  return (
+    <FlatList
+      keyExtractor={item => item.key}
+      ListFooterComponent={footerProps => (
+        <FormButton {...footerProps} title="Load More..." onPress={onPressFooter} />
+      )}
+      renderItem={({ item }) => <ListItem {...item} />}
+      {...props}
+    />
+  );
+}
+
+export default List;
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,5 +1,83 @@
-import * as React from 'react';
+import { LayoutAnimation, RefreshControl } from "react-native";
 
-import { AppPageContainer } from '../components';
+import Firebase from "../config/Firebase";
+import List from "../components/List";
+import React from "react";
 
-export default (props) => <AppPageContainer {...props} heading="Feed" />
+const PAGE_SIZE = 5;
+
+class FeedScreen extends React.Component {
+  state = {
+    loading: false,
+    posts: [],
+    data: {}
+  };
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  // Append the item to our states `data` prop
+  addPosts = posts => {
+    this.setState(previousState => {
+      let data = {
+        ...previousState.data,
+        ...posts
+      };
+      return {
+        data,
+        // Sort the data by timestamp
+        posts: Object.values(data).sort((a, b) => a.timestamp < b.timestamp)
+      };
+    });
+  };
+
+  // Call our database and ask for a subset of the user posts
+  makeRemoteRequest = async lastKey => {
+    // If we are currently getting posts, then bail out..
+    if (this.state.loading) {
+      return;
+    }
+    this.setState({ loading: true });
+
+    // The data prop will be an array of posts, the cursor will be used for pagination.
+    const { data, cursor } = await Firebase.shared.getPaged({
+      size: PAGE_SIZE,
+      start: lastKey
+    });
+
+    this.lastKnownKey = cursor;
+    // Iteratively add posts
+    let posts = {};
+    for (let child of data) {
+      posts[child.key] = child;
+    }
+    this.addPosts(posts);
+
+    // Finish loading, this will stop the refreshing animation.
+    this.setState({ loading: false });
+  };
+
+  _onRefresh = () => this.makeRemoteRequest();
+
+  onPressFooter = () => this.makeRemoteRequest(this.lastKnownKey);
+
+  render() {
+    LayoutAnimation.easeInEaseOut();
+
+    return (
+      <List
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.loading}
+            onRefresh={this._onRefresh}
+          />
+        }
+        onPressFooter={this.onPressFooter}
+        data={this.state.posts}
+      />
+    );
+  }
+}
+
+export default FeedScreen;
