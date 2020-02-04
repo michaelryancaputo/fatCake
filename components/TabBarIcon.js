f18a79839afef0286f15f9a7c902e261ce4733da
Michael Caputo <mike@capu.to>
2020-02-04 04:22:24 -0800

    Fix reanimated dep

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -2,17 +2,20 @@ import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import { Icon } from 'native-base';
+import styled from 'styled-components/native';
+
+const StyledIcon = styled(Icon)`
+  margin-bottom: -3;
+  font-size: 20;
+  color: ${props => props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+`;
 
 export default function TabBarIcon(props) {
   return (
-    <Icon
+    <StyledIcon
       type="Ionicons"
       name={props.name}
-      style={{
-        marginBottom: -3,
-        fontSize: 20,
-        color: props.focused ? Colors.tabIconSelected : Colors.tabIconDefault
-      }}
+      focused={props.focused}
     />
   );
 }
